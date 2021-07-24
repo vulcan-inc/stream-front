bfb154a6a3198900eba4466bd99a26eece68ce8f
Ravi Lamichhane <ravilamichhane30@gmail.com>
2021-07-24 11:53:44 +0545

    ingame

--- src/components/MapData/TeamCard.jsx
+++ src/components/MapData/TeamCard.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { storageUrl } from '../../env'
 import styles from './teamcard.module.css'
-const TeamCard = ({rank , team}) => {
+const TeamCard = ({ rank, team }) => {
     return (
         <div className={styles.teamcard}>
             <div className={styles.rank}>{rank}</div>
             <div className={styles.team_image_box}>
-                <img src={storageUrl+team.image} alt="" height={50} width={50} />
+                <img src={storageUrl + team.image} alt="" height={50} width={50} />
             </div>
             <div className={styles.team_info}>
                 <div className={styles.team_info_name}>{team.name}</div>
